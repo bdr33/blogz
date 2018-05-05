dcbb5b057a009b51c1530ac233efe94ff8706fc2
Brittany Robinson <bdr3300@gmail.com>
2018-05-05 11:41:04 -0500

    Finished!

--- main.py
+++ main.py
@@ -2,17 +2,12 @@ from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 import cgi
 import os
-import jinja2
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:launchcode@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-
-
-template_dir= os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env= jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 class Blog(db.Model):
 
@@ -36,19 +31,16 @@ def blog():
     if request.args.get('id'):
         ind_id = request.args.get('id')
         blog=Blog.query.get(ind_id)
-        template = jinja_env.get_template('ind_entry.html')
-        return template.render(blog=blog)
+        return render_template('ind_entry.html',blog=blog)
     
     else:
         blogs=Blog.query.all()
-        template = jinja_env.get_template('bloghome.html')
-        return template.render(blogs=blogs)
+        return render_template('bloghome.html',blogs=blogs, page_header='Build a Blog')
 
 
 @app.route("/newpost")
 def newpost():
-    template = jinja_env.get_template('newpost.html')
-    return template.render()
+    return render_template('newpost.html')
 
 @app.route("/validate-post", methods=['POST'])
 def validate_post():
@@ -60,14 +52,14 @@ def validate_post():
     content_error= ''
     
     if title=="":
-        title_error = 'You must enter a title'
+        title_error = 'Please fill in the title'
     else: 
         if len(title) > 120:
             title_error= 'Your title may not be more than 120 characters long'
         
         
     if content=="":
-        content_error = 'You must enter content'
+        content_error = 'Please fill in the content'
     else: 
         if len(content) > 5000:
             content_error= 'Your content may not be more than 5000 characters'     
@@ -81,8 +73,7 @@ def validate_post():
         return redirect(new_page)
 
     else: 
-        template = jinja_env.get_template('newpost.html')
-        return template.render(title=title, content=content,title_error=title_error, 
+        return render_template('newpost.html',title=title, content=content,title_error=title_error, 
         content_error=content_error)
 
 
