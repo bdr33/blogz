6b4151233e8a1786c6c4e1173ff19934a36cc260
Brittany Robinson <bdr3300@gmail.com>
2018-05-05 15:57:32 -0500

    login page

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:launchcode@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:launchcode@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -14,10 +14,22 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     content = db.Column(db.String(5000))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, content):
+    def __init__(self, title, content, author):
         self.title = title
         self.content = content
+        self.author = author
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username= db.Column(db.String(25), unique=True)
+    password = db.Column(db.String(25))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 
 
@@ -37,7 +49,6 @@ def blog():
         blogs=Blog.query.all()
         return render_template('bloghome.html',blogs=blogs, page_header='Build a Blog')
 
-
 @app.route("/newpost")
 def newpost():
     return render_template('newpost.html')
@@ -56,8 +67,7 @@ def validate_post():
     else: 
         if len(title) > 120:
             title_error= 'Your title may not be more than 120 characters long'
-        
-        
+          
     if content=="":
         content_error = 'Please fill in the content'
     else: 
@@ -75,8 +85,30 @@ def validate_post():
     else: 
         return render_template('newpost.html',title=title, content=content,title_error=title_error, 
         content_error=content_error)
+@app.route("/login", methods = ['POST'])
+def login():
+    return render_template('login.html')
 
+@app.route("/validate-login", methods = ['POST', 'GET'])
+def validate_login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form ['password']
+        #clarify this
+        user = User.query.filter_by(username=username).first()
+        users = User.query.all()
 
+        if user not in users: 
+            username_error = 'User does not exist'
+
+        if user.password != password:
+            password_error = 'Password is incorrect'
+        
+        if not username_error and not password_error:
+            session['username'] = username
+            return redirect ('/newpost')
+        else:
+            return render_template('login.html', username_error=username_error, password_error=password_error)
 
 if __name__ == '__main__':
     app.run()
